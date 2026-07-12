8b091f2c915685afda9ca83dc43d1c43de47ef6a
Prasadaware8125 <prasadaware6613@gmail.com>
2026-07-12 21:17:28 +0530

    Learning DP part 2

---
+++ 34_Dynamic_Programming_Part_2/Knapsack_0_1_Recursion.java
@@ -0,0 +1,28 @@
+
+public class Knapsack_0_1_Recursion {
+    public static int knapsack(int[] val, int[] wt, int W, int n) {
+        if( W == 0 || n == 0 ) {
+            return 0;
+        }
+
+        if( wt[n-1] <= W ) {    // Case 1: valid
+            // include
+            int ans1 = val[n-1] + knapsack(val, wt, W-wt[n-1], n-1);
+
+            // exclude
+            int ans2 = knapsack(val, wt, W, n-1);
+
+            return Math.max(ans1, ans2);
+        } else {    // Case 2: invalid
+            return knapsack(val, wt, W, n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int[] val = {15, 14, 10, 45, 30};
+        int[] wt = {2, 5, 1, 3, 4};
+        int W = 7;
+        int n = val.length;
+
+        System.out.println("Max profit = "+knapsack(val, wt, W, n));
+    }
+}
